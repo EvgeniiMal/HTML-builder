c178f37632d839fd5817f8f64b114c7e17754493
Mishel Kasianova <mishel.kasyanova.7@ukr.net>
2021-11-09 01:25:44 +0200

    date

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,17 +1,28 @@
 let fs = require('fs');
-// fs.mkdir(`${__dirname}/files-copy`, err => {
-//   if(err) throw err; 
-//   console.log('Папка успешно создана');
-// });
 
+fs.stat(`${__dirname}/files-copy`, function(err) {
+    if (!err) {
+        console.log('Директория есть');
+    }
+    else if (err.code === 'ENOENT') {
+        fs.mkdir(`${__dirname}/files-copy`, err => {
+            if(err) throw err; 
+            console.log('Папка успешно создана');
+          });
+    }
+});
 console.log(__dirname);
 let way = __dirname+'\\'+'files';
 fs.readdir(way, (err, data) => {
     console.log(way);
     data.forEach(file => {
       console.log(file);
+      fs.createWriteStream(__dirname+'\\files-copy\\'+file);
+      fs.copyFile('04-copy-directory\\files\\'+file, __dirname+'\\files-copy\\'+file, (err) => {
+        if (err) throw err;
+        console.log('source.txt was copied to destination.txt');
+    });
     });
   });
 
-  console.log(__dirname+'\\files-copy\\test-js.js');
-const writeStream = fs.createWriteStream(__dirname+'\\files-copy\\test-js.js');
+
