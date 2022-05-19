59eea0f6e15c761c166766f6e2463d58ce08beb9
Katya Ruzieva <katsiarynaruziyeva@gmail.com>
2022-05-19 10:55:37 +0300

    fix:fix files in folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const pathFolder = path.join(__dirname, 'secret-folder');
+fs.readdir(pathFolder,{withFileTypes: true}, (err, files) => {
+  files.forEach(file => {
+    if(file.isDirectory()===false){
+      let arr=Object.values(file);
+      let arr2=arr.toString().split('.');
+      const pathFolder2 = path.join(pathFolder, arr.toString());
+      let name=arr2[0];
+      let name2=arr2[1];
+      let size=(fs.statSync(pathFolder2).size)/1000;
+      console.log(`${name} - ${name2} - ${size}kb`);
+    }});
+});
