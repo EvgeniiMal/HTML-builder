be6a2ad4736cdaf389ee4945784bb79e6cf8a3ce
Katya Ruzieva <katsiarynaruziyeva@gmail.com>
2022-05-19 11:14:10 +0300

    fix:fix stat mistake

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const {stat}=require('fs');
 const pathFolder = path.join(__dirname, 'secret-folder');
 fs.readdir(pathFolder,{withFileTypes: true}, (err, files) => {
   files.forEach(file => {
@@ -9,7 +10,8 @@ fs.readdir(pathFolder,{withFileTypes: true}, (err, files) => {
       const pathFolder2 = path.join(pathFolder, arr.toString());
       let name=arr2[0];
       let name2=arr2[1];
-      let size=(fs.statSync(pathFolder2).size)/1000;
-      console.log(`${name} - ${name2} - ${size}kb`);
+      // let size=(fs.statSync(pathFolder2).size)/1000;
+      fs.stat(pathFolder2,(err,stat)=>console.log(`${name} - ${name2} - ${stat.size/1000}kb`));
+      // console.log(`${name} - ${name2} - ${size}kb`);
     }});
 });
