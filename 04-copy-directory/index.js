eab02b629849db170a3b89e871bc31f3fb0d3d3b
Katya Ruzieva <katsiarynaruziyeva@gmail.com>
2022-05-19 12:13:32 +0300

    fix:fix copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,14 @@
-
+const fs = require('fs');
+const path = require('path');
+const fsPromises = fs.promises;
+const pathFolder2 = path.join(__dirname, 'files');
+const pathFolder = path.join(__dirname, 'files-copy');
+fsPromises.mkdir(pathFolder, {recursive:true});
+fs.readdir(pathFolder2, (err, files) => {
+  if(err) throw err;
+  files.forEach(file=>
+    fs.copyFile(path.join(pathFolder2, file), path.join(pathFolder, file), err => {
+      if(err) throw err; 
+      console.log('Файл успешно скопирован');
+    }));
+});
