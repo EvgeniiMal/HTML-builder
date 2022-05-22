344a4aa0ba1cd560aefb5bf8c1f6a0d8e7a40847
Roman Maklakov <mclakov@MacBook-Air-Roman.local>
2022-05-23 00:50:43 +0300

    feat: 2 task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,21 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 const myPath = path.join(__dirname, './text.txt');
-
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 });
-readline.question('jjj', (name) => {
-  console.log(name);
-
-  fs.appendFileSync(myPath, name);
+const writableStream = fs.createWriteStream(myPath, 'utf8');
+const byeMessage = 'DONE! File text.txt created with you message!';
 
 
+console.log('Write some text!');
+readline.on('line', (data) => {
 
-
-
-
-
-  readline.close();
+  if (data === 'exit') {
+    readline.close();
+    console.log(byeMessage);
+  } else {
+    writableStream.write(data + '\n');
+  }
 });
+
+readline.on('SIGINT', () => {
+  readline.close();
+  console.log(byeMessage);
+});
+
+
+
