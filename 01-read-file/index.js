ec55c2ad6dda30f3fc6ec42ddd89756b9ec6c85e
Roman Maklakov <mclakov@MacBook-Air-Roman.local>
2022-05-22 21:04:16 +0300

    fix: read file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 // const path = require('path');
-// const stream = require('stream');
 
 
-fs.readFile('01-read-file/text.txt', 'utf8',
-  function(error,data){
 
-    if(error) throw error;
-    console.log(data);
-  });
+
+
+let readableStream = fs.createReadStream('01-read-file/text.txt', 'utf8');
+
+readableStream.on('data', function(chunk){
+  console.log(chunk);
+});
