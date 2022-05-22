7a26bcf410839c74e9b90a7edc3544de63d80e0e
Roman Maklakov <mclakov@MacBook-Air-Roman.local>
2022-05-22 20:29:46 +0300

    feat: read file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,2 +1,11 @@
 const fs = require('fs');
-const path = require('path');
+// const path = require('path');
+// const stream = require('stream');
+
+
+fs.readFile('01-read-file/text.txt', 'utf8',
+  function(error,data){
+
+    if(error) throw error;
+    console.log(data);
+  });
