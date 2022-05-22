9cc86fae12b0c08c10584b0c0943b04be01d8b0d
Andrew Pisliakov <pislakov@gmail.com>
2022-05-22 18:07:12 +0300

    feat: first task has done

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,3 +1,10 @@
 const fs = require('fs');
+const path = require('path');
 
-fs.ReadStream();
+const currentPath = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(currentPath, 'utf-8');
+let view = '';
+
+readStream.on('data', chunk => view += chunk);
+readStream.on('end', () => console.log(view));
+readStream.on('error', error => console.log('Error', error.message));
