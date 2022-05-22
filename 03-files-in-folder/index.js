9418981edf78afc253dd94bf15165ab14967f2ba
Roman Maklakov <mclakov@MacBook-Air-Roman.local>
2022-05-23 02:40:35 +0300

    fix: 3 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,23 +2,15 @@ const fs = require('fs');
 const path = require('path');
 const myPath = path.join(__dirname, 'secret-folder');
 
-
 fs.readdir(myPath, (err, data) => {
   if (err) throw err;
-
   data.map(elem => {
-
-
     let pathToFile = path.join(myPath, elem);
-    // console.log(pathToFile);
     fs.stat(pathToFile, (err, stats) => {
       if (err) throw err;
       if (stats.isFile()) {
-        console.log(path.parse(pathToFile).name,'-', path.extname(pathToFile).slice(1),'-',stats.size);
+        console.log(path.parse(pathToFile).name,'-', path.parse(pathToFile).ext.slice(1),'-',stats.size);
       }
-
-
     });
   });
-
 });
