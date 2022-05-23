796fddc779ee34614379286707099e3f887f8403
Roman Maklakov <mclakov@MacBook-Air-Roman.local>
2022-05-24 01:02:39 +0300

    feat: 5 task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,2 +1,41 @@
 const fs = require('fs');
 const path = require('path');
+const pathToStyle = path.join(__dirname, 'styles');
+const pathToDist = path.join(__dirname, 'project-dist');
+
+
+
+
+fs.readdir(pathToStyle, (err, data) => {
+  if (err) throw err;
+  data.map(elem => {
+    let pathToFile = path.join(pathToStyle, elem);
+    fs.stat(pathToFile, (err, stats) => {
+      if (err) throw err;
+      if (stats.isFile() && path.parse(pathToFile).ext === '.css') {
+        fs.readFile(pathToFile, 'utf8', (err, data) => {
+          if (err) throw err;
+          fs.readdir(pathToDist, err2 => {
+            if (err2) throw err2;
+            fs.appendFile(path.join(pathToDist, 'bundle.css'), data, err1 => {
+              if (err1) throw err1;
+            });
+          });
+        });
+      }
+    });
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
