732aeed2478eb80ea8a1a02301ff4aacf2554d37
Andrey Bazlov <buzzlove@mail.ru>
2022-05-24 00:50:13 +0500

    feat: finished 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+let fs = require('fs');
+
+fs.readdir('./04-copy-directory/', (err, files) => {
+    if(err) throw err; // не прочитать содержимое папки 
+    if(!files.includes("files copy")){
+        createDirectory();
+    } else {
+        copyFiles();
+    };
+ });
+
+ function createDirectory(){
+    fs.mkdir('./04-copy-directory/files copy/', err => {if(err) throw err; copyFiles();}); 
+ }
+
+ function copyFiles(){
+    fs.readdir('./04-copy-directory/files/', (err, files) => {
+        if(err) throw err; 
+        files.forEach(item => copyFile(item));
+     });
+     function copyFile(item){
+        fs.copyFile(`./04-copy-directory/files/${item}`, `./04-copy-directory/files copy/${item}`, (err) => {if (err) throw err;});
+     }
+ }
+
 
