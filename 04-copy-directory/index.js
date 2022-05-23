f70a69b74f044fcf0b7ac7d089ec171d177a27ff
Roman Maklakov <mclakov@MacBook-Air-Roman.local>
2022-05-23 21:59:07 +0300

    feat: 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,3 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 const pathToFiles = path.join(__dirname, 'files');
+const pathToCopyFiles = path.join(__dirname, 'files-copy/');
+
+
+fs.mkdir(pathToCopyFiles, {recursive: true},err => {
+  if (err) throw err;
+});
+
+
+fs.readdir(pathToFiles, (err, data) => {
+  if (err) throw err;
+  data.map(elem => {
+    fs.copyFile(path.join(pathToFiles, elem), path.join(pathToCopyFiles, elem), err => {
+      if (err) throw err;
+    });
+  });
+});
+
+
