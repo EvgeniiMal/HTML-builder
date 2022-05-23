162520a7efa1c8a8f4b906b85e9acef22328f765
annTerry <annak@1gb.ru>
2022-05-23 23:34:54 +0300

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const fs = require('fs');
+const path = require('node:path');
 
+const sourceFolder = path.join(__dirname,'files');
+const targetFolder = path.join(__dirname,'files-copy');
+
+
+function copyDir ( oldDir, newDir )
+{
+  fs.mkdir(newDir, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+  fs.readdir(oldDir, { withFileTypes: true },(err, files) => {
+    if (err) console.log(err);
+    else {
+      files.forEach(file => {  
+        if (file.isFile())
+        {   
+          fs.copyFile (path.join(oldDir,file.name),path.join(newDir,file.name),(err) =>
+          {
+            if (err) console.log(err);    
+          });  
+        }
+        else if (file.isDirectory())
+        {
+          copyDir ( path.join(oldDir,file.name), path.join(newDir,file.name) );
+        }
+      });}}); 
+}
+
+copyDir ( sourceFolder, targetFolder );
