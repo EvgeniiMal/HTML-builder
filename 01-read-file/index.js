226c6eb0ff4efafad7f5a55bee3149808d3fec46
@olga_sidoruk <olya.sidoruk98@gmail.com>
2022-10-28 22:08:19 +0300

    feat: implement first task

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs =  require('fs')
+const path = require('path')
+const { stdout } = require('process')
+
+const filePath = path.join(__dirname, 'text.txt')
+
+const readableStream = fs.createReadStream(filePath)
+readableStream.on('data', data => stdout.write(data))
