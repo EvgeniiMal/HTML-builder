b5961d3f26a5df68624aeb01f76978aa2a2ffc1d
@olga_sidoruk <olya.sidoruk98@gmail.com>
2022-10-29 14:53:35 +0300

    feat: implement task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const path = require('path');
+const { mkdir, copyFile, readdir, unlink } = require('fs/promises');
 
+const folderPath = path.join(__dirname, 'files');
+const newFolderPath = path.join(__dirname, 'files-copy');
+
+async function copyFilesFromFolder() {
+  try {
+    await mkdir(newFolderPath, { recursive: true });
+    const filesInNewFolder = await readdir(newFolderPath);
+    const filesToCopy = await readdir(folderPath);
+    if (filesInNewFolder.length) {
+      for (const file of filesInNewFolder) {
+        await unlink(path.join(newFolderPath, file));
+      }
+    }
+    for (const file of filesToCopy) {
+      await copyFile(path.join(folderPath, file), path.join(newFolderPath, file));
+    }
+  } catch (err) {
+    console.error('Произошла ошибка: ', err);
+  }
+}
+copyFilesFromFolder();
