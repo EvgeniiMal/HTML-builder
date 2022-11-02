6412fb1ccc8c149243b40c66299e1f84851fe93b
Andrey Scerbina <scerbina2016@gmail.com>
2022-11-02 10:35:47 +0200

    feat: 03-files-in-folder works

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var fs = require('fs');
+//var async = require('async');
+let dir='./03-files-in-folder/secret-folder/';
+function directoryInn(put){
+    let fileSize;
+fs.readdir(put,{withFileTypes: true},function(err, items) {   
+
+   for (let i=0; i<items.length; i++) {
+    fs.stat(put+items[i].name,function(err, bum){
+      if(items[i].isFile()){
+      console.log(path.parse(items[i].name).name+' - '+
+      (path.extname(put+items[i].name).toString()).replace('.','')+' - '+Number(bum.size)/1000+'kb')};
+      if(items[i].isDirectory()){ directoryInn(put+items[i].name+'/')};
+      });
+     
+    };
+       
+});
+};
+directoryInn(dir);
