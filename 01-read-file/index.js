2154058639882c79523f91daefce84d7613de862
Peter Terentev <petrfalkovsky@yandex.ru>
2022-11-04 19:22:31 +0300

    feat(read): remaked task, added handler errors n sortter funcion

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,12 +1,16 @@
 const fs = require("fs");
 const path = require("path");
-const { stdout } = process;
 
 const file = path.join(__dirname, ("text.txt"));
 const encode = "utf-8";
-let content = "";
-
 const stream = fs.createReadStream(file, encode);
 
-stream.on("data", chunk => content += chunk);
-stream.on("end", () => stdout.write(content));
+async function readNOut() {
+    try {
+        stream.pipe(process.stdout);
+    }
+    catch (error) {
+        console.log("something wrong happened, dear");
+    }
+}
+readNOut();
