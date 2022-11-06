73c2c95fc9a332d6c38e40dec55d16d6ca085173
ChristinaKarlovich <christina.karlovich@gmail.com>
2022-11-07 01:47:48 +0300

    feat: implement method to copy files from folder to copy folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const { stdout, stderr } = require("process");
+const { mkdir, readdir, copyFile } = require("fs/promises");
+const path = require("path");
 
+const dirNameCurrent = path.dirname(__filename);
+const dirNameTarget = path.join(dirNameCurrent, "files");
+const dirNameTargetCopy = path.join(dirNameCurrent, "files-copy");
+
+copyDir();
+
+async function copyDir() {
+  try {
+    const createDir = await mkdir(dirNameTargetCopy, { recursive: true });
+    if (createDir) {
+      stdout.write(`files-copy folder was created\n`);
+    }
+
+    const files = readdir(dirNameTarget, { withFileTypes: true });
+    files.then((data) => {
+      data
+        .filter((file) => file.isFile())
+        .forEach((file) => {
+          copyFile(
+            path.join(dirNameTarget, file.name),
+            path.join(dirNameTargetCopy, file.name)
+          );
+          stdout.write(
+            `files\\${file.name} was copied to files-copy\\${file.name}\n`
+          );
+        });
+    });
+  } catch (err) {
+    stderr.write(err.message);
+  }
+}
