25d3b0329188f711ec9878a0f1901e10d309f0a1
ivan52945 <ivan52945@gmail.com>
2022-11-06 19:02:46 +0300

    feat: solve files in folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const fs = require('fs');
+const { stdin, stdout } = process;
+const stream = require('stream');
+
+const dirPath = path.resolve(__dirname, 'secret-folder');
+
+const files = fs.promises.readdir(dirPath, { encoding: "utf-8", withFileTypes: true });
+files.then(function (files) {
+	for (let file of files) {
+		if (!file.isFile()) continue;
+
+		const pathFile = path.resolve(__dirname, 'secret-folder', file.name);
+
+		const point = file.name.indexOf(".");
+		if (point == -1) continue;
+
+		const fileName = file.name.slice(0, point);
+		const fileExt = file.name.slice(point + 1, file.name.length)
+		const stat = fs.stat(pathFile, (error, stat) => {
+			console.log(`${fileName}--${fileExt}--${stat.size / 1000}kb`);
+		});
+	}
+})
