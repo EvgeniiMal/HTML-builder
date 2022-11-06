8a549d98a26844e513a96b5da73dd81dbd540af3
julia <yusiagerachka@gmail.com>
2022-11-06 20:49:16 +0300

    feat: add task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fsPr = require("fs/promises");
+const path = require("node:path");
 
+const initFolderPath = "04-copy-directory/files";
+const copiedFolderPath = "04-copy-directory/files-copy";
+
+async function cleanCopiedFolder(folderPath) {
+  for (const file of await fsPr.readdir(folderPath)) {
+    await fsPr.unlink(path.join(folderPath, file));
+    console.log(file, " deleted");
+  }
+}
+async function copyFolder(initFolderPath, copiedFolderPath) {
+  fsPr.access(copiedFolderPath).catch(async () => {
+    await fsPr.mkdir(copiedFolderPath, { recursive: true });
+  });
+  await cleanCopiedFolder(copiedFolderPath);
+  const files = await fsPr.readdir(initFolderPath);
+  files.forEach(async (file) => {
+    const initFilePath = path.resolve(initFolderPath, file),
+      copiedFilePath = path.resolve(copiedFolderPath, file);
+    await fsPr.copyFile(initFilePath, copiedFilePath);
+    console.log(file, " copied");
+  });
+}
+
+copyFolder(initFolderPath, copiedFolderPath);
