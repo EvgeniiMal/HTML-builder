febca2e7ecbfcc9a7e9d59eb4d989ddaa56d9293
Ihar Skavysh <30604865+Igogoblin@users.noreply.github.com>
2022-11-07 19:51:13 +0300

    refactor: add explanation in forth file

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,18 +1,20 @@
 
 const  path = require("path");
 const fs = require("fs");
+// вводим промисы, чтобы в дальнейшем их не писать
 const fsPromises = fs.promises;
 
 const dirSrc = "./04-copy-directory/files/";
 const dirDest = "./04-copy-directory/files-copy/";
-
+// используем метод для асинхронного создания каталога fs.mkdir(путь, режим, обратный вызов)
 fsPromises.mkdir(dirDest, {
   recursive: true
 },(err) => {
+
   if (err) {
     console.log(err)};
   });
-
+// метод для взаимодействия с жестким диском
 fs.readdir(dirSrc,{withFileTypes: true},function(err, item) {   
   
   for (let i=0; i<item.length; i++) {
