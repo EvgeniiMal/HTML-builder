d69bec8f62721b84fe161239ac8232826bb15d07
Ihar Skavysh <30604865+Igogoblin@users.noreply.github.com>
2022-11-07 19:39:52 +0300

    refactor: add explanation

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,13 +2,14 @@ const fs = require('fs')
 const path = require('path')
 
 fs.promises.readdir(path.join(__dirname, './secret-folder'), {withFileTypes: true})
+// перебираем файлы
 .then(files => files.forEach(element => {
     if (element.isFile()) {
       fs.stat(path.join(__dirname, './secret-folder', element.name), (err, stats) => {
         if(err) {
          return console.error(err) 
         }
-        
+//         выводим в консоль согласно  формата
         console.log(`${element.name.split('.')[0]} - ${element.name.split('.')[1]} - ${Number(stats.size / 1024).toFixed(3)}kb`)
       });
     }
