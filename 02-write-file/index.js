dacedb61ed35f2d6eaab7c8868e14cc31664c918
Takethisworld <62119373+Takethisworld@users.noreply.github.com>
2022-11-08 03:23:56 +0600

    feat:add script fo thirst 3 subjects

---
+++ 02-write-file/index.js
@@ -0,0 +1,28 @@
+const {stdin,stdout}=process;
+const fs=require('fs');
+const path=require('path');
+
+const pathWrite = path.join(__dirname,'text.txt');
+
+const createWriteFs= fs.createWriteStream(pathWrite)
+
+const endMessage=()=>{
+stdout.write('File text.txt already created')
+process.exit()}
+
+stdout.write('Plese write text\n');
+
+stdin.on('data',data=>{
+    
+    const dataString=data.toString().trim()
+
+    if(dataString === 'exit'){
+        endMessage()
+    }else{
+        createWriteFs.write(data)
+    }
+})
+
+process.on('SIGINT',()=>process.exit())
+
+process.on('exit',()=>stdout.write('File text.txt already created'))
