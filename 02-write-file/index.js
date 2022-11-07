20fb5aba66ee65eb921088a73c32ef1f10c7c439
Xenia Kasperovich <heiligxenia@gmail.com>
2022-11-07 23:13:56 +0300

    feat: done task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+const process = require("process");
+const { exit, stdin } = process;
+
+fs.writeFile(path.join("02-write-file", "text.txt"), "\n", (err) => {
+  if (err) throw err;
+  console.log("Файл был создан");
+});
+
+stdin.on("data", (data) => {
+  fs.appendFile(path.join("02-write-file", "text.txt"), data, (err) => {
+    if (err) throw err;
+    console.log("Файл был изменен");
+  });
+
+  if (data.toString().trim() === "exit") {
+    console.log("Good bye");
+    process.exit();
+  }
+
+  process.on("SIGINT", () => {
+    console.log("Good bye");
+    process.exit();
+  });
+});
