e8581461a13be65d48754259db35c2b05878347d
Xenia Kasperovich <heiligxenia@gmail.com>
2022-11-07 23:42:52 +0300

    feat: done task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,43 @@
+const fs = require("fs");
+const path = require("path");
 
+fs.mkdir(path.join("04-copy-directory", "files-copy"), (err) => {
+  if (err) throw err; // не удалось создать папку
+  console.log("Папка успешно создана");
+});
+
+fs.copyFile(
+  path.join("04-copy-directory", "files", "test-css.css"),
+  path.join("04-copy-directory", "files-copy", "test-css.css"),
+  (err) => {
+    if (err) throw err; // не удалось скопировать файл
+    console.log("Файл успешно скопирован");
+  }
+);
+
+fs.copyFile(
+  path.join("04-copy-directory", "files", "test-image.jpg"),
+  path.join("04-copy-directory", "files-copy", "test-image.jpg"),
+  (err) => {
+    if (err) throw err; // не удалось скопировать файл
+    console.log("Файл успешно скопирован");
+  }
+);
+
+fs.copyFile(
+  path.join("04-copy-directory", "files", "test-js.js"),
+  path.join("04-copy-directory", "files-copy", "test-js.js"),
+  (err) => {
+    if (err) throw err; // не удалось скопировать файл
+    console.log("Файл успешно скопирован");
+  }
+);
+
+fs.copyFile(
+  path.join("04-copy-directory", "files", "test-text.txt"),
+  path.join("04-copy-directory", "files-copy", "test-text.txt"),
+  (err) => {
+    if (err) throw err; // не удалось скопировать файл
+    console.log("Файл успешно скопирован");
+  }
+);
