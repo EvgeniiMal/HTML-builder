b0339ca98fe6304c37e09fceff265358d5af65c7
Takethisworld <62119373+Takethisworld@users.noreply.github.com>
2022-11-09 23:28:07 +0600

    fix:write script for copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require("fs");
+const { readdir, copyFile, constants } = require("fs/promises");
+const path =require('path');
+const fsPromises = fs.promises
+
+const pathFile = path.resolve(__dirname,'files')
+
+const desForCopy=path.resolve(__dirname,'files-copy')
+
+fsPromises.mkdir(desForCopy,{recursive:true},(err)=>{
+    if(err) throw err
+});
+
+async function readCopyFile(){
+    const readOriginPath = await readdir(pathFile)
+    for(const files of readOriginPath){
+        console.log(files)
+        let originPath = path.resolve(pathFile,files)
+        let copyPath = path.resolve(desForCopy,files)
+        await copyFile(originPath,copyPath,constants.COPYFILE_EXCL)
+    }
+    
+}
+readCopyFile()
 
