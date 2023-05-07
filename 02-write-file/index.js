0c94cf727de1f395a9ca08b3be41b2a66d6b25fe
warlock150@gmail.com <email@example.com>
2023-05-07 09:51:01 +0200

    task 01, 02, 03 is OK

---
+++ 02-write-file/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('node:path');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+const pathToFile = path.dirname(__filename) + '\\text.txt';
+
+const question = `What do you think about? Press "enter" and input what do you think'.
+After end of the string press Enter. To finish type 'exit' and press "enter"`;
+const goodBye = 'Thank you very much';
+
+let writeStream = fs.createWriteStream(pathToFile, 'utf8');
+
+rl.question(question, (answer) => {
+  rl.on('line', (answer) => (answer == 'exit') ? process.exit() : writeStream.write(answer + '\n'));
+});
+
+process.on('exit', () => console.log(goodBye));
---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('node:path');
+let pathToFile = path.dirname(__filename) + '/secret-folder';
+
+fs.readdir(pathToFile,
+  (err, files) => {
+  console.log("\nCurrent directory files:");
+  if (err)
+    console.log(err);
+  else {
+    files.forEach(file => {
+      fs.stat(`${pathToFile}/${file}`, (err, stats) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        if (stats.isFile()) {
+        console.log(`${path.basename(file, path.extname(file))} - ${path.extname(file).slice(1)} - ${stats.size} bytes`);
+        }
+      });
+    });
+  }
+});
