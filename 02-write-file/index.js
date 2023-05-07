336aefc88483d579764015c9a4d6438e7ee30840
natalliadk <sunny-1@mail.ru>
2023-05-07 17:44:54 +0300

    finish task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const writeFile = () => {
+  const filePath = path.join(__dirname, './text.txt');
+
+  const file = fs.createWriteStream(filePath, { flags: 'a' });
+
+  process.stdin.on('data', (data) => { 
+    if (data.toString().trim() === 'exit') {
+      console.log('Bye...'); 
+      process.exit();
+    }
+    file.write(data);
+  });
+
+  process.on('SIGINT', () => {
+    console.log('\n');
+    console.log('Bye...');
+    process.exit(); 
+  }); 
+};
+
+writeFile();
+  
