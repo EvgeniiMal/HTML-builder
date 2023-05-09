cd18ed7e6327820306a1d7c9c6cd4de307dc42f0
Masha Vilenchyk <mariavilenchyk@gmail.com>
2023-05-09 09:48:22 +0300

    fix: fix js-function(08.05.2023 00.14)

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -10,16 +10,18 @@ fs.appendFile(pathTXT, "", function (err) {
   console.log("Введите, пожалуйста, текст в консоль!");
   let stdin = process.openStdin();
   stdin.on("data", function (chunk) {
+    if (chunk.toString().trim() == "exit") {
+      console.log("Благодарю, что сделали проверку этого задания!");
+      process.exit(0);
+    }
     fs.appendFile(pathTXT, chunk, function (err) {
       if (err) throw err;
-      
     });
-    console.log("Got chunk: " + chunk);
 
-    process.on('SIGINT', function () {
-        console.log('Благодарю, что сделали проверку этого задания!');
-        process.exit(0);
+    console.log("Got chunk: " + chunk);
+    process.on("SIGINT", function () {
+      console.log("Благодарю, что сделали проверку этого задания!");
+      process.exit(0);
     });
   });
-
 });
